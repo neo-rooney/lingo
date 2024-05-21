b79daf20a25955047d3b34b5f9ba3c350ed51d00
rooney <bch3454@gmail.com>
2024-05-21 11:31:12 +0900

    feat: Project setup
    - Setup Nextjs(https://nextjs.org/docs/getting-started/installation)
    - Setup shadcn ui(https://ui.shadcn.com/docs/installation/next)

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="text-green-500 font-bold text-20xl">
+      <Button size="lg">Click Me</Button>
+    </div>
+  );
+}
