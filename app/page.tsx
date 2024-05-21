8a1a7f8cdb727437b55389f0a5d97e865993732a
rooney <bch3454@gmail.com>
2024-05-21 12:03:06 +0900

    feat: Marketing Skeleton
    - flex 정리(https://www.notion.so/flex-1-1-0-69cf3d1a5f164df0b060c1a63ac53a4f?pvs=4)

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div className="text-green-500 font-bold text-20xl">
-      <Button size="lg">Click Me</Button>
-    </div>
-  );
-}
